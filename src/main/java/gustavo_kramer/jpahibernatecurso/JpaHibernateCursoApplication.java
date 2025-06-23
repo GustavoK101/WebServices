b004aa195277fccfe34f4c572e4bf27445aa183f
GustavoK101 <gustavokramer123@gmail.com>
2025-06-23 09:31:08 -0300

    feat: first commit

---
+++ src/main/java/gustavo_kramer/jpahibernatecurso/JpaHibernateCursoApplication.java
@@ -0,0 +1,13 @@
+package gustavo_kramer.jpahibernatecurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gustavo_kramer/jpahibernatecurso/JpaHibernateCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package gustavo_kramer.jpahibernatecurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
