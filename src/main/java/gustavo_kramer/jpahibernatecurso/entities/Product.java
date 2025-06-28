203c77cafc2402e2121d98a03f88b21d6f4a01bd
GustavoK101 <gustavokramer123@gmail.com>
2025-06-28 14:40:46 -0300

    feat: Many-to-many association with JoinTable Product and Category

--- src/main/java/gustavo_kramer/jpahibernatecurso/entities/Product.java
+++ src/main/java/gustavo_kramer/jpahibernatecurso/entities/Product.java
@@ -20,6 +20,9 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
@@ -73,7 +76,7 @@ public class Product implements Serializable {
         this.imgUrl = imgUrl;
     }
 
-    private Set<Category> getCategories() {
+    public Set<Category> getCategories() {
         return categories;
     }
     
